f7352269b55b3d91fd711cfff46d2247b3a8db99
Red Importante <redimportante13@gmail.com>
2023-09-03 02:07:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Initialize count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+
+    // Change count value based on which button was clicked
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    // Change count color for positive and negative
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+
+    value.textContent = count;
+  });
+});
